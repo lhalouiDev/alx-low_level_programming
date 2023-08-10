2fac2cb300b0358a82d8d794b59dd586b5c688e5
lhalouiDev <abdelali.lhaloui@yahoo.com>
2023-08-10 09:59:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,18 +9,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
-
-5-printf.c
-
-#include<stdio.h>
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
