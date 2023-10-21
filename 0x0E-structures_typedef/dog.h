1e7122ebbb7e68ce45b1c8117dd2d6467ab7cd81
lhalouiDev <abdelali.lhaloui@yahoo.com>
2023-10-21 12:22:25 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
 #ifndef DOG_H 
 #define DOG H
+
 /**
-struct dog - a dog struct
-*name: dog's name 
-*age: dog's age
-*owner: owner's name
+*struct dog - a dog struct
+*@name: dog's name 
+*@age: dog's age
+*@owner: owner's name
 *
 * Description: just a lone dog struct in a big kitty world
 */
