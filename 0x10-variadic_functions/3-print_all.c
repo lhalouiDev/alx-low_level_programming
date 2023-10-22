9452bd17e52690087d3eef1cc10aa997596d8dff
lhalouiDev <abdelali.lhaloui@yahoo.com>
2023-10-22 05:28:49 -0700

    a

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,50 @@
-#include "variadic_functions.h"
-	#include <stdarg.h>
-	#include <stdio.h>
+#include <stdarg.h>
+#include <stdio.h>
 
+/**
+ * print_all - Prints anything based on a format
+ * @format: A format string containing types of arguments
+ * @...: The variable number of arguments to print
+ */
+void print_all(const char * const format, ...)
+{
+    va_list args;
+    char *str;
+    int i = 0;
 
-	/**
-	 * print_all - prints anything
-	 * @format: list of types of arguments passed to the function
-	 */
-	void print_all(const char * const format, ...)
-	{
-		int i = 0;
-		char *str, *sep = "";
+    va_start(args, format);
 
+    while (format && format[i])
+    {
+        switch (format[i])
+        {
+            case 'c':
+                printf("%c", va_arg(args, int));
+                break;
+            case 'i':
+                printf("%d", va_arg(args, int));
+                break;
+            case 'f':
+                printf("%f", va_arg(args, double));
+                break;
+            case 's':
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(nil)";
+                printf("%s", str);
+                break;
+            default:
+                i++;
+                continue;
+        }
 
-		va_list list;
+        if (format[i + 1] != '\0')
+            printf(", ");
 
+        i++;
+    }
 
-		va_start(list, format);
-
-
-		if (format)
-		{
-			while (format[i])
-			{
-				switch (format[i])
-				{
-					case 'c':
-						printf("%s%c", sep, va_arg(list, int));
-						break;
-					case 'i':
-						printf("%s%d", sep, va_arg(list, int));
-						break;
-					case 'f':
-						printf("%s%f", sep, va_arg(list, double));
-						break;
-					case 's':
-						str = va_arg(list, char *);
-						if (!str)
-							str = "(nil)";
-						printf("%s%s", sep, str);
-						break;
-					default:
-						i++;
-						continue;
-				}
-				sep = ", ";
-				i++;
-			}
-		}
-
-
-		printf("\n");
-		va_end(list);
-	
+    va_end(args);
+    printf("\n");
+}
 
